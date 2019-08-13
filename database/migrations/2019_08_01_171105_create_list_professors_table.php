3009c97b7a255a46c30af1cab6542b204f9c4ce0
Eduardo Zavala López <estadias.sa@utt.ads>
2019-08-13 16:34:56 -0700

    Actualizacion de curriculum profesor

--- database/migrations/2019_08_01_171105_create_list_professors_table.php
+++ database/migrations/2019_08_01_171105_create_list_professors_table.php
@@ -14,6 +14,7 @@ class CreateListProfessorsTable extends Migration
     public function up()
     {
         Schema::create('list_professors', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->bigIncrements('id');            
             $table->string('full_name');            
             $table->string('email');            
